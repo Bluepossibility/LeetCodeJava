3efe41e313ff5a4eb645160d27f3e9abe2be0271
Hanshan <hl3515@columbia.edu>
2023-11-10 23:41:54 -0500

    resolved 1743

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,17 +1,11 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
+import TestData.TestData1743;
+
+import Solutions.*;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        assert (Solution1743.restoreArray(TestData1743.input)) == TestData1743.output;
     }
 }
---
+++ src/src/TestData/TestData1743.java
@@ -0,0 +1,7 @@
+package TestData;
+
+public class TestData1743 {
+
+    public static int[][] input = {{4,-2}, {1,4},{-3,1}};
+    public static int[] output = {-2,4,1,-3};
+}
