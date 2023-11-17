82357bd61105ec8084a3bf4ed47d0c4af905837d
Hanshan <hl3515@columbia.edu>
2023-11-16 22:11:53 -0500

    resolved 346

---
+++ src/src/Solutions/Solution346.java
@@ -0,0 +1,28 @@
+package Solutions;
+
+import java.util.*;
+
+public class Solution346 {
+
+    public static class MovingAverage{
+
+        int size;
+        int windowSum = 0;
+        int count = 0;
+        Deque<Integer> queue = new ArrayDeque<>();
+
+        public MovingAverage(int size) {
+            this.size = size;
+        }
+
+        public double next(int val) {
+           count++;
+           queue.add(val);
+           int tail = count > size ? (int) queue.poll() : 0;
+
+           windowSum = windowSum - tail + val;
+
+           return windowSum * 1.0 / Math.min(size, count);
+        }
+    }
+}
