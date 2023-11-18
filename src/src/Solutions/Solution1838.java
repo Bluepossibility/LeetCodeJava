e97a5244140e849cff09dae6cc9fa70e0bf4d0e4
Hanshan <hl3515@columbia.edu>
2023-11-18 00:27:48 -0500

    finetuned solution 1838

--- src/src/Solutions/Solution1838.java
+++ src/src/Solutions/Solution1838.java
@@ -7,21 +7,18 @@ public class Solution1838 {
     public static int maxFrequency(int[] nums, int k) {
         Arrays.sort(nums);
         int left = 0;
-        int ans = 0;
-        long curr = 0;
+        long windowSum = 0;
 
         for (int right = 0; right < nums.length; right++){
             int target = nums[right];
-            curr += target;
+            windowSum += target;
 
-            while ((right - left + 1) * target - curr > k){
-                curr -= nums[left];
-                left++;
+            if ((long)(right - left + 1) * target - windowSum > k){
+                windowSum -= nums[left];
+                left ++;
             }
-
-            ans = Math.max(ans, right - left + 1);
         }
 
-        return ans;
+        return nums.length - left + 1;
     }
 }
