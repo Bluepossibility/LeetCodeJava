28593a83d54f3049e8325a362080016faeaaa609
Hanshan <hl3515@columbia.edu>
2023-11-21 20:43:42 -0500

    resolved 1424

--- src/src/Main.java
+++ src/src/Main.java
@@ -3,9 +3,10 @@ import TestData.TestData1743;
 
 import Solutions.*;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(Solution1814.countNicePairs(new int[] {13,10,35,24,76}));
     }
 }
