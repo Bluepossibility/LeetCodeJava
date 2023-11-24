754a7e5010dc56e476abfb93c48e8a8e3b4617a4
Hanshan Li <hl3515@columbia.edu>
2023-11-23 23:55:41 -0500

    resolved 1561

---
+++ src/src/Solutions/Solution1561.java
@@ -0,0 +1,14 @@
+package Solutions;
+
+import java.util.Arrays;
+
+public class Solution1561 {
+    public int maxCoins(int[] piles) {
+        Arrays.sort(piles);
+        int ans = 0;
+        for (int i = piles.length/3; i < piles.length; i += 2){
+            ans += piles[i];
+        }
+        return ans;
+    }
+}
