419797db8b409be2ffebfa3434375279f1ef3b53
Hanshan <hl3515@columbia.edu>
2023-12-04 21:56:04 -0500

    resolved 1688

--- src/src/Main.java
+++ src/src/Main.java
@@ -7,6 +7,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Solution1662.arrayStringsAreEqual(new String[]{"ab", "c"}, new String[]{"a", "bc"});
+        Solution1688.numberOfMatches(7);
     }
 }
