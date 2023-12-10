d79d1b96faf3b0764df0c6d65b9b6ba616a62b6c
Hanshan <hl3515@columbia.edu>
2023-12-10 18:02:12 -0500

    resolved 606

---
+++ src/src/Solutions/Solution606.java
@@ -0,0 +1,36 @@
+package Solutions;
+
+public class Solution606 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {
+
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;this.left = left;
+            this.right = right;
+        }
+     }
+
+    public String tree2str(TreeNode root) {
+        if (root.left == null && root.right == null){
+            return String.valueOf(root.val);
+        }
+        else if (root.left != null && root.right != null){
+            return root.val + "(" + tree2str(root.left) + ")" + "(" + tree2str(root.right) + ")";
+        }
+        else if (root.left != null){
+            return root.val + "(" + tree2str(root.left) + ")";
+        }
+        else {
+            return root.val + "()(" + tree2str(root.right) + ")";
+        }
+    }
+}
